ebbe8b82585393bd026d50d14ffad64e6fe67298
Davidlohr Bueso <davidlohr.bueso@hp.com>
2013-09-11 14:26:22 -0700

    ipc,shm: cleanup do_shmat pasta
    commit f42569b1388b1408b574a5e93a23a663647d4181 upstream.
    
    Clean up some of the messy do_shmat() spaghetti code, getting rid of
    out_free and out_put_dentry labels.  This makes shortening the critical
    region of this function in the next patch a little easier to do and read.
    
    Signed-off-by: Davidlohr Bueso <davidlohr.bueso@hp.com>
    Tested-by: Sedat Dilek <sedat.dilek@gmail.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Manfred Spraul <manfred@colorfullife.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- ipc/shm.c
+++ ipc/shm.c
@@ -1196,11 +1196,15 @@ out_nattch:
 		shm_destroy(ns, shp);
 	else
 		shm_unlock(shp);
+<<<<<<< HEAD
 	up_write(&shm_ids(ns).rwsem);
 	return err;
 
+	up_write(&shm_ids(ns).rw_mutex);
+	return err;
+
 out_unlock:
-	rcu_read_unlock();
+	shm_unlock(shp);
 out:
 	return err;
 }
